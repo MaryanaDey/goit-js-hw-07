73c20f38f6f1da0ad15c7dc37aad4c95c3f72b70
MaryanaDey <81640002+MaryanaDey@users.noreply.github.com>
2021-09-14 13:25:18 +0300

    начало

---
+++ js/task-02.js
@@ -0,0 +1,15 @@
+const ingredients = [
+    'Картошка',
+    'Грибы',
+    'Чеснок',
+    'Помидоры',
+    'Зелень',
+    'Приправы',
+  ];
+
+  const  = document.createElement("ingredients");
+  const food = ingredients.forEach(ingredient=> {
+      const items = document.createElement("li");
+      ulList.appendChild(items)
+  });
+  console.log(food);
